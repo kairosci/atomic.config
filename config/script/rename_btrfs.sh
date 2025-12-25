8f19c94bb62e0cbeeaa8f6f6d5b161472f59be05
Alessio <alessio.attilio@engineer.com>
2025-12-25 23:42:57 +0100

    refactor: encapsulate BTRFS label renaming logic into functions and add progress output.

--- config/script/rename_btrfs.sh
+++ config/script/rename_btrfs.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/bash
 set -e
 
-btrfs filesystem label /var fedora
-btrfs filesystem label /var/home fedora
+# Function to rename BTRFS labels
+rename_btrfs_labels() {
+    echo "Renaming BTRFS labels..."
+    btrfs filesystem label /var fedora
+    btrfs filesystem label /var/home fedora
+    echo "BTRFS labels renamed."
+}
+
+# Main execution
+main() {
+    rename_btrfs_labels
+}
+
+main
