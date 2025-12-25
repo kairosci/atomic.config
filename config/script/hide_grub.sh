bc72e26f3e16b447f5337bb81ed733112f929a69
Alessio <alessio.attilio@engineer.com>
2025-12-25 23:42:49 +0100

    refactor: encapsulate script logic into functions and add logging to `rename_btrfs.sh` and `hide_grub.sh`

--- config/script/hide_grub.sh
+++ config/script/hide_grub.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/bash
 set -e
 
-cp /boot/grub2/grub.cfg /boot/grub2/grub.cfg.bak
-sed -i 's/^set timeout=.*/set timeout=0/' /boot/grub2/grub.cfg
-chmod 444 /boot/grub2/grub.cfg
+# Function to hide GRUB menu
+hide_grub() {
+    echo "Hiding GRUB menu..."
+    cp /boot/grub2/grub.cfg /boot/grub2/grub.cfg.bak
+    sed -i 's/^set timeout=.*/set timeout=0/' /boot/grub2/grub.cfg
+    chmod 444 /boot/grub2/grub.cfg
+    echo "GRUB menu hidden."
+}
+
+# Main execution
+main() {
+    hide_grub
+}
+
+main
