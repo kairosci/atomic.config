9628a35235c796fbd682aa8062d03e306ed3994c
Alessio <alessio.attilio@engineer.com>
2026-01-01 21:44:21 +0100

    feat: add Antigravity RPM repository configuration

--- config/script/set_rpm.sh
+++ config/script/set_rpm.sh
@@ -100,6 +100,16 @@ install_packages() {
 
 
 install_google_antigravity() {
+
+    echo "Adding Antigravity repo"
+    sudo tee /etc/yum.repos.d/antigravity.repo << EOL
+    [antigravity-rpm]
+    name=Antigravity RPM Repository
+    baseurl=https://us-central1-yum.pkg.dev/projects/antigravity-auto-updater-dev/antigravity-rpm
+    enabled=1
+    gpgcheck=0
+EOL
+
     echo "Installing Antigravity"
     rpm-ostree install --idempotent antigravity
     echo "Antigravity installed"
